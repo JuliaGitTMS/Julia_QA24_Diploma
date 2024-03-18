93ef605c19deab0fde9c89db4450ee891f082e80
vitalshadrin <vital.shadrin2@gmail.com>
2024-03-18 22:51:03 +0300

    added fixes and improvments

--- src/main/java/pageObjects/ShoppingCartPage.java
+++ src/main/java/pageObjects/ShoppingCartPage.java
@@ -1,28 +1,31 @@
 package pageObjects;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import pageObjects.baseObjects.BaseLoadedPage;
 
+import java.util.List;
 
 import static java.lang.Double.parseDouble;
 import static java.lang.Integer.parseInt;
 
 public class ShoppingCartPage extends BaseLoadedPage<ShoppingCartPage> {
+
+    private final By price = By.xpath(".//div[contains(@class, '_wrapper')]/span");
     private final By productAmount = By.className("ui-checkout-cart__products-count");
-    private final By productList = By.xpath("//div[@class='_product_ko34a_8']");
+    private final By productList = By.xpath("//div[contains(@class,'cart__item')]");
     private final By itemQuantity = By.xpath("//div[@aria-label='Изменить количество товара']/div");
-    private final By itemCurrentPrice = By.xpath("//span[@class='_currentPrice_1v56e_50']/span");
-    private final By itemDiscountPrice = By.xpath("//span[@class='_discountPrice_1v56e_54']/span");
     private final By increaseItemQuantity = By.xpath("//button[@aria-label='Увеличить']");
     private final By decreaseItemQuantity = By.xpath("//button[@aria-label='Уменьшить']");
     private final By deleteAllItems = By.className("_inner_13so9_2");
     private final By deleteItem = By.className("ui-checkout-cart__item-remove-from-cart");
     private final By totalPrice = By.xpath("(//div[@class='_value_1oots_21']/span)[2]");
     private final By checkout = By.className("ui-checkout__button-submit");
+    private By getProduct(Integer productIndex) {
+        return By.xpath("(//div[contains(@class,'cart__item')])[" + productIndex + "]");
+    }
 
     public ShoppingCartPage checkProductsQuantity() {
         int sum = 0;
@@ -37,23 +40,15 @@ public class ShoppingCartPage extends BaseLoadedPage<ShoppingCartPage> {
         return parseInt(driver.findElements(productList).get(index).findElement(itemQuantity).getText());
     }
 
-    public Double getItemCost(int index) {
-        try {
-            return parseDouble(driver.findElements(productList).get(index).findElement(itemCurrentPrice).getText()
-                    .substring(0, driver.findElements(productList).get(index).findElement(itemCurrentPrice).getText().indexOf("р")).replaceAll(" ", ""));
-
-        } catch (NoSuchElementException e) {
-            return parseDouble(driver.findElements(productList).get(index).findElement(itemDiscountPrice).getText()
-                    .substring(0, driver.findElements(productList).get(index).findElement(itemDiscountPrice).getText().indexOf("р")).replaceAll(" ", ""));
-
-        }
+    public Double getItemCost(Integer productIndex) {
+        List<WebElement> prices = driver.findElement(getProduct(productIndex + 1)).findElements(price);
+        return parseDouble(prices.get(prices.size() == 1 ? 0 : 1).getText().split(" ")[0]);
     }
 
-    public ShoppingCartPage checkProductPrice(int index, Double price) {
-        Assert.assertEquals(getItemCost(index), getItemQuantity(index) * price);
+    public ShoppingCartPage checkProductPrice(int productIndex, Double price) {
+        Assert.assertEquals(getItemCost(productIndex), price);
         return me();
     }
-
 
     public ShoppingCartPage addOneMoreItem(int index) {
         if (driver.findElements(productList).get(index).findElement(increaseItemQuantity).isEnabled()) {
